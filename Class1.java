2a7c975786d5963a9fe1eeb5f949429a6b456fc9
swathi <swathikalari1402@gmail.com>
2023-01-27 22:08:30 +0530

    first commit

---
+++ Class1.java
@@ -0,0 +1,17 @@
+
+package pacakage1;
+
+public class Class1 {
+	public static void main(String[]args)
+	{
+		Class1 a1 =new Class1();
+		a1.method1();
+	}
+	public void method1()
+	{
+		System.out.println("java program");
+		
+	}
+	
+
+}
